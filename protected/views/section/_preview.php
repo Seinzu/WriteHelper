ec06acead638ba81f0ea52d57f9480250334f2a7
Andrew Rumble <andrewjrumble@gmail.com>
2011-01-04 22:51:52 +0000

    removing forced line breaks as causing a formatting problem

--- protected/views/section/_preview.php
+++ protected/views/section/_preview.php
@@ -7,9 +7,10 @@ $textData = $texts->getData();
 if (!empty($textData)){
 	$output = '';
 	foreach ($textData as $text){
-		$output .= $this->renderPartial('//text/_preview', array('data'=>$text), true) . '<br /><br />';
+		$output .= $this->renderPartial('//text/_preview', array('data'=>$text), true);
 	}
 	$Markdown = new CMarkdown;
+	$Markdown->purifyOutput = true;
 	$word_count = str_word_count(strip_tags($output));
 	?>
 	<h2><?php echo $data->title;?><span class='statistic'>(Word Count: <?php echo $word_count;?> words - approx )</span></h2>
