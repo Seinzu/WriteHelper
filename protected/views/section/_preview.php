5d81a488bf4cfb70b8519062c6c2c1680c3b7fe7
Andrew Rumble <andrewjrumble@gmail.com>
2011-01-04 22:20:37 +0000

    added a per section word count

--- protected/views/section/_preview.php
+++ protected/views/section/_preview.php
@@ -1,5 +1,5 @@
 
-<h2><?php echo $data->title;?></h2>
+
 <?php
 
 $texts = new CActiveDataProvider('Text', array('criteria'=>array('condition'=>'section=' . $data->id)));;
@@ -10,7 +10,10 @@ if (!empty($textData)){
 		$output .= $this->renderPartial('//text/_preview', array('data'=>$text), true) . '<br /><br />';
 	}
 	$Markdown = new CMarkdown;
-	
+	$word_count = str_word_count($output);
+	?>
+	<h2><?php echo $data->title;?><span class='statistic'>(Word Count: <?php echo $word_count;?> words )</span></h2>
+	<?php 
 	echo $Markdown->transform($output);
 }
 
