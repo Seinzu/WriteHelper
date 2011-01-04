353bd44142d40172ecd56994c90c0c385d16ef60
Andrew Rumble <andrewjrumble@gmail.com>
2011-01-04 22:30:54 +0000

    making word count more accurate by stripping tags

--- protected/views/section/_preview.php
+++ protected/views/section/_preview.php
@@ -10,9 +10,9 @@ if (!empty($textData)){
 		$output .= $this->renderPartial('//text/_preview', array('data'=>$text), true) . '<br /><br />';
 	}
 	$Markdown = new CMarkdown;
-	$word_count = str_word_count($output);
+	$word_count = str_word_count(strip_tags($output));
 	?>
-	<h2><?php echo $data->title;?><span class='statistic'>(Word Count: <?php echo $word_count;?> words )</span></h2>
+	<h2><?php echo $data->title;?><span class='statistic'>(Word Count: <?php echo $word_count;?> words - approx )</span></h2>
 	<?php 
 	echo $Markdown->transform($output);
 }
